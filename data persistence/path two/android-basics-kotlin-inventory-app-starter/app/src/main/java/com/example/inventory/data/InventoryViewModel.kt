f34d309005b9ea3523a56041b76d8c77c3551c74
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2022-10-28 16:16:01 +0300

    Create InventoryViewModel.kt
    Add view Model

---
+++ data persistence/path two/android-basics-kotlin-inventory-app-starter/app/src/main/java/com/example/inventory/data/InventoryViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.inventory.data
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class InventoryViewModel(private val itemDao: ItemDao): ViewModel() {
+    /*Populate the InventoryViewModel class to add inventory data to the database.
+    Observe the Item entity and Add Item screen in the Inventory app.*/
+    private fun insertItem(item: Item) {
+        /*To interact with the database off the main thread, start a coroutine
+        and call the DAO method within it*/
+        viewModelScope.launch {
+            itemDao.insert(item)
+        }
+    }
+
+    private fun getNewItemEntry(itemName: String, itemPrice: String, itemCount: String): Item {
+        return Item(
+            itemName = itemName,
+            itemPrice =  itemPrice.toDouble(),
+            quantityInStock = itemCount.toInt()
+        )
+    }
+
+    private fun addNewItem(itemName: String,itemPrice: String, itemCount: String) {
+       val newItem = getNewItemEntry(itemName, itemPrice,itemCount)
+        insertItem(newItem)
+    }
+
+
+
+
+}
+/*view model instantiation*/
+class InventoryViewModelFactory(private val itemDao: ItemDao): ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(InventoryViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return InventoryViewModel(itemDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
